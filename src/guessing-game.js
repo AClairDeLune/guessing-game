21f452468e2c01e82337b945c041b1952627c863
aclairdelune <anastasiya9girko@mail.ru>
2022-08-16 02:20:40 +0300

    feat:add changes

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.result = Math.round((this.max + this.min) / 2);
+      return this.result;
     }
-
+  
     lower() {
-
+      this.max = this.result;
     }
-
+  
     greater() {
-
+      this.min = this.result;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
